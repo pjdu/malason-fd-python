77060f549b7da95543922a8f7bcea07315ff21ef
Malason <whatever1988@live.cn>
2018-09-18 20:24:34 +0800

    Malason

--- stock/check/rank.py
+++ stock/check/rank.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 import warnings
+from multiprocessing import Pool
 
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
@@ -12,7 +13,6 @@ warnings.simplefilter('ignore')
 import os
 import sys
 import platform
-import pandas as pd
 
 # 使用insert 0即只使用github，避免交叉使用了pip安装的abupy，导致的版本不一致问题
 sys.path.insert(0, os.path.abspath('../'))
@@ -27,7 +27,7 @@ from abupy import ABuSymbolPd
 abupy.env.disable_example_env_ipython()
 
 sysstr = platform.system()
-if(sysstr =="Windows"):
+if (sysstr == "Windows"):
     data_source = "C:\\Users\\dell\\abu\\data\\csv"
 else:
     data_source = "/root/abu/data/csv"
@@ -50,24 +50,40 @@ def get_file_fist(dir, ex_type):
 def calcChange(a, b):
     return round((b - a) / a * 100, 2)
 
+
+def cal_stock_change(stock, start_time, end_time):
+    rank_info = stock.split('_')
+    stock_symbol = rank_info[0]
+#df = ABuSymbolPd.make_kl_df(stock_symbol, n_folds=8)[start_time:end_time]
+   # if not df.empty:
+    return {"symbol": stock_symbol, "range": 100}
+
+
+def list_of_groups(init_list, children_list_len):
+    list_of_groups = zip(*(iter(init_list),) * children_list_len)
+    end_list = [list(i) for i in list_of_groups]
+    count = len(init_list) % children_list_len
+    end_list.append(init_list[-count:]) if count != 0 else end_list
+    return end_list
+
+
 # 全市场回测，股票涨幅排名
 def cal_stock_rank(ex_type, start_time, end_time):
-    symbols = []
-    ranges = []
-    sotck_list = get_file_fist(data_source, ex_type)
-    for stock in sotck_list:
-        rank_info = stock.split('_')
-        stock_symbol = rank_info[0]
-        df = ABuSymbolPd.make_kl_df(stock_symbol, n_folds=8)[start_time:end_time]
-        if not df.empty:
-            symbols.append(stock_symbol)
-            ranges.append(calcChange(df['close'][0], df['close'][-1]))
+    stock_list = get_file_fist(data_source, ex_type)
 
-    stock_rank = pd.Series(ranges, index=symbols)
-    stock_rank.sort_values(ascending=False, inplace=True)
+    pool = Pool(500)
+    result = []
+    for stock in stock_list:
+        result.append(pool.apply(func=cal_stock_change, args=(stock, start_time, end_time,)))
 
-    return stock_rank
+    pool.close()
+    pool.join()
+
+    print(result)
+
+    return result
 
 
-stock_rank = cal_stock_rank(sys.argv[1], sys.argv[2], sys.argv[3])
-print(stock_rank)
+#stock_rank = cal_stock_rank(sys.argv[1], sys.argv[2], sys.argv[3])
+stock_rank = cal_stock_rank("us", "2018-09-01", "2018-09-17")
+
