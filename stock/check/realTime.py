13feb1315c6d8c5fc202b9d914f1e8c618a89dcf
Malason <whatever1988@live.cn>
2018-09-19 12:59:13 +0800

    Malason

--- stock/check/realTime.py
+++ stock/check/realTime.py
@@ -1,5 +1,19 @@
+# pip install terminaltables
 import tushare as ts
+import numpy as np
+from terminaltables import AsciiTable
 
-df = ts.get_realtime_quotes(['600188', '600763'])
+df = ts.get_index()
+head = list(df)
+nr = df.values.tolist()
+c=np.vstack((head, nr)).tolist()
+table = AsciiTable(c)
+print(table.table)
+print('\n')
 
-print(df[['code','name','price','bid','ask','volume','amount','time']])
+df = ts.get_realtime_quotes(['600188', '600763'])[['code','name','price','bid','ask','volume','amount','time']]
+head = list(df)
+nr = df.values.tolist()
+c=np.vstack((head, nr)).tolist()
+table = AsciiTable(c)
+print(table.table)
