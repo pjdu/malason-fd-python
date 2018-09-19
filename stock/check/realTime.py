b08be607265a35b19b424c0c626797cbed71bbc4
Malason <whatever1988@live.cn>
2018-09-19 13:18:19 +0800

    Malason

--- stock/check/realTime.py
+++ stock/check/realTime.py
@@ -11,7 +11,7 @@ table = AsciiTable(c)
 print(table.table)
 print('\n')
 
-df = ts.get_realtime_quotes(['600188', '600763'])[['code','name','price','bid','ask','volume','amount','time']]
+df = ts.get_realtime_quotes(['600188', '600763', '512010', '601012'])[['code', 'name', 'pre_close', 'open', 'price', 'bid', 'ask', 'volume', 'amount', 'high', 'low', 'date', 'time']]
 head = list(df)
 nr = df.values.tolist()
 c=np.vstack((head, nr)).tolist()
