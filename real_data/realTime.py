a29d0611f625303ed813c8147ca0a1e8772cc691
Malason <whatever1988@live.cn>
2019-02-27 01:13:51 +0800

    Malason

--- real_data/realTime.py
+++ real_data/realTime.py
@@ -1,4 +1,8 @@
 import tushare as ts
 
+from common import print_df
+
 df = ts.get_realtime_quotes(['601888', '512010', '601012'])[
     ['code', 'name', 'pre_close', 'open', 'price', 'bid', 'ask', 'volume', 'amount', 'high', 'low', 'date', 'time']]
+
+print_df(df)
