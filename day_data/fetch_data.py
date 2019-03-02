e572ace74b0e337d6c861adab76aa5f55a3055ea
Malason <whatever1988@live.cn>
2019-03-02 23:46:23 +0800

    Malason

--- day_data/fetch_data.py
+++ day_data/fetch_data.py
@@ -11,27 +11,34 @@ import time
 ts.set_token('9c4af04257e55b3f490d14ac46c00cd71383ed0846d8e10694907926')
 pro = ts.pro_api()
 
-data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-
-table_name = 'cn_stock_list'
-database = 'mysql+pymysql://root:hefei168lj@localhost/malason?charset=utf8'
-
-engine = create_engine(database)
-data['range'] = None
-data.to_sql('m_fd_data_' + table_name, engine, if_exists='replace')
-
-startDate = '20190101'
+startDate = '20100101'
 endDate = time.strftime('%Y%m%d', time.localtime(time.time()))
+
+database = 'mysql+pymysql://root:hefei168lj@localhost/malason?charset=utf8'
+engine = create_engine(database)
+
+
+def fetch_trade_cal():
+    trade_cal = pro.trade_cal(exchange='', start_date=startDate, end_date=endDate)
+    trade_cal.to_sql('m_fd_data_trade_cal', engine, if_exists='replace')
+
+
+def fetch_stock():
+    data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+    data.to_sql('m_fd_data_cn_stock_list', engine, if_exists='replace')
+
 
 def fetch_task(symbol):
     df = ts.pro_bar(pro_api=pro, ts_code=symbol, adj='qfq', start_date=startDate, end_date=endDate)
     if df is not None:
         df.to_sql('m_fd_data_' + symbol, engine, index=False, if_exists='replace')
 
-def main():
+
+def batch_fetch_data():
+    data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
     with ThreadPoolExecutor(10) as executor:
         for item in data['ts_code']:
             executor.submit(fetch_task, item)
 
-if __name__ == '__main__':
-    main()
+
+fetch_trade_cal()
