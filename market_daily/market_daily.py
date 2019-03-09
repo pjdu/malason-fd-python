3109ca038608c09afa9f8007ad3b9ad8eb21d5db
Malason <whatever1988@live.cn>
2019-03-09 23:33:44 +0800

    Malason

--- market_daily/market_daily.py
+++ market_daily/market_daily.py
@@ -1,7 +1,7 @@
 import sys, os
 import tushare as ts
 import time
-from pyecharts import Line
+from pyecharts import Line, Page
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -21,12 +21,13 @@ stock_list = [{'ts_code': '601012.SH', 'ts_name': "隆基股份"},
               {'ts_code': '300274.SZ', 'ts_name': "阳光电源"},
               {'ts_code': '600438.SH', 'ts_name': "通威股份"}]
 
-line = Line("光伏板块", width=1600, height=800)
-
+page = Page()
 for stock in stock_list:
+    line = Line(stock['ts_name'], width=1600, height=800)
     df = ts.pro_bar(pro_api=pro, ts_code=stock['ts_code'], adj='qfq', start_date=startDate, end_date=endDate)
     if df is not None:
         df = df.sort_index(ascending=True)
         line.add(stock['ts_name'], df['trade_date'], df['close'], mark_point=["max", "min"])
+        page.add_chart(line)
 
-line.render('market_daily.html')
+page.render('market_daily.html')
